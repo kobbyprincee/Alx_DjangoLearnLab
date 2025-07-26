ebebb8bd6a1b26324e6f33f5afb5ea4bd278a5b0
William Akoto <akotowilliam92@gmail.com>
2025-07-26 19:46:38 +0000

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Author(models.Model):
@@ -50,3 +51,4 @@ class UserProfile(models.Model):
             ("can_change_book", "Can change a book"),
             ("can_delete_book", "Can delete a book"),
         ]
+
