f4684c97a14de06ed9ab8478e1fe7f4e8c0d507f
William Akoto <akotowilliam92@gmail.com>
2025-08-17 12:01:26 +0000

    Initial commit

---
+++ django_blog/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
