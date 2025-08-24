97ffe876eab572790300769f1f7aa830f44b6c68
William Akoto <akotowilliam92@gmail.com>
2025-08-24 10:29:39 +0000

    Add api  urls to social_media_api api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView, LikePostView, UnlikePostView
 
@@ -8,6 +8,7 @@ router.register('comments', CommentViewSet, basename='comment')
 
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),
     path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
     path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
